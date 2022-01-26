21a69da92663f74e05c4d9aca816e915d675432b
Larry Motuzis <larrymotuzis@gmail.com>
2022-01-25 20:04:36 -0600

    commit tree 43f86137e8c81675ff51133d8825434c6018d580

--- index.js
+++ index.js
@@ -1,1 +1,91 @@
-// Code your solution here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Code your solution here
+
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+
+// // function findMatching(arrayofDrivers, name) {
+// //     const nameArray = arrayofDrivers.filter(
+// //         function(item){
+// //             if (item.toLowerCase() === name.toLowerCase()){
+// //                 return true
+// //             }
+
+// //         }   
+// //     )   
+// //     return nameArray
+// // }
+// // console.log(findMatching(drivers, 'Sarah'))
+
+// function findMatching(arrayOfDrivers, driver) {
+//     const filtetDrivers = arrayOfDrivers.filter(
+//         function(user) {
+//             if (user.toUpperCase() === driver.toUpperCase()){
+//                 return true
+//             }  
+//         }         
+//     )  
+//     return filtetDrivers
+// }
+
+
+
+
+// function fuzzyMatch(arrayOfDrivers, driverName) {
+//     let firstTwoLetters = driverName.slice(0, 1);
+//      let driverLooper = arrayOfDrivers.filter(
+//         function(eachDriver) {
+//         let oneDriver = eachDriver.slice(0, 1);
+//         if (oneDriver === firstTwoLetters) {
+//             return true
+//             }
+//         }
+//     )  
+//         return driverLooper
+// }   
+
+// console.log(fuzzyMatch(drivers, 'Bo'))
+
+
+// function matchName(driverObjectsArray,nameToCompare){
+//     const loopThrunFindName = driverObjectsArray.filter(
+//         function(currentDriverObj){
+//             if (currentDriverObj.name === nameToCompare){
+//                 return true
+//             }
+
+//         }
+//     )     
+//         return loopThrunFindName  
+// }           
